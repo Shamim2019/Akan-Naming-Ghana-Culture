f9a34cf5c96751f3a74a4494b0027978651cb3cf
Shamim maloba <shamimmaloba@gmail.com>
2021-11-13 16:02:29 +0300

    added function

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let male names=['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+
+let female names=['Akosou','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+
+let days of the week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+function akanNaming(get.ElementById=date ) {
+    
+}
