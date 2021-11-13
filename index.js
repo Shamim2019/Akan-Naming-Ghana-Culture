aae55c889174794f081d9fdff6d5cf9af87492ab
Shamim maloba <shamimmaloba@gmail.com>
2021-11-13 21:22:29 +0300

    added week_days

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 
-let male names=['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+let maleNames=['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame']
 
-let female names=['Akosou','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+let femaleNames=['Akosou','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 
-let days of the week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-
-function akanNaming(get.ElementById=date ) {
-    
-}
+let days_of_week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
