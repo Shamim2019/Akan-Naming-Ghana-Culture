79089d9d955de6c4198302ffde31daf4ca389879
Shamim maloba <shamimmaloba@gmail.com>
2021-11-14 14:01:07 +0300

    added if statement

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ let maleNames=['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame']
 let femaleNames=['Akosou','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 
 let days_of_week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+function akanNaming(){
+let date=document.getElementById('date').value
+let gender=document.getElementById('gender').value
+ let dayNumber=new Date(days_of_week).getDay()
+
+
+ if gender==('female'){
+     console.log['when born'](days_of_week)[dayNumber]
+
+     ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+ }
+    
+ 
+  
+}
