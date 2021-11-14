d35daf3a61f800cc3ced70628c80f94202aba637
Shamim maloba <shamimmaloba@gmail.com>
2021-11-14 17:56:10 +0300

    added else and else if

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ let femaleNames=['Akosou','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 
 let days_of_week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
-function akanNaming(){
+function akanNaming(event){
+    event.preventDefault()
 let date=document.getElementById('date').value
 let gender=document.getElementById('gender').value
- let dayNumber=new Date(days_of_week).getDay()
+ let dayNumber=new Date(date).getDay();
 
-
- if gender==('female'){
-     console.log['when born'](days_of_week)[dayNumber]
-
-     ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+ if(gender='' || date == '' ){
+     alert('Error! You have to provide both gender' )
  }
-    
- 
-  
+
+ else if(gender=='female'){
+    console.log('Born on $ {days_of_week[dayNumber]}
+    'and my Akanname is $ { femaleAkanNames[dayNumber]})
+
+  else {
+// else (gender=='male'){
+    console.log('Born on ${days_of_week[dayNumber]}
+    and my Akanname is ${maleAkanNames[dayNumber]}')
 }
+
