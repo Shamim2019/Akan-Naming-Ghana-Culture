66e5859ecc5c8c78b76e84653c4f0391de9069d4
Shamim maloba <shamimmaloba@gmail.com>
2021-11-15 22:27:51 +0300

    added modified files

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 
  let maleNames=
- ['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+  ['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame']
 
- let femaleNames=
+  let femaleNames=
  ['Akosou','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 
- let days_of_week=
- ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+  let weekDays=
+  ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
     
-function akanNaming(event){
-     event.preventDefault()
-    let date=document.getElementById('date').value
-     let gender=document.getElementById('gender').value
-        let days_of_week=new Date(date).getDay()
 
-  if(gender='' || date == '' ){
-           alert('Error! You have to provide both gender' )}
+function akanNaming(event) {
+    event.preventDefault();
+     let gender = document.getElementById('gender').value
+    let date = document.getElementById('date').value
+    // let name=document.getElementById('name').value
+     let dayNumber = new Date("date").getDay(); 
 
-  if(gender=='female'){
-     alert(`Born on ${days_of_week[days_of_week]}
-     and my Akanname is${femaleNames[days_of_week]}`)
+     if (gender == '' || date == '') {
+        alert('Error! You have to provide both your gender and name')
+     }
 
-
-      elseif(gender=='male');{
-    alert(`Born on${days_of_week[days_of_week]}
-     and my Akanname is${maleNames[days_of_week]}`)
- }}
+    else if(gender=='male'){
+         alert(`born on${weekDays[dayNumber]}
+         and your akanname is ${maleNames[dayNumber]}`)
+    }
+    else  { 
+        alert(`born on${weekDays[dayNumber]}
+       and your akanname is ${femaleNames[dayNumber]}`)
+    }
 }
